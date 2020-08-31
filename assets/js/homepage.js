42cd5a508573e94fda4830a2c010464ea308ac98
Jordan Carver <jordan_carver@hotmail.com>
2020-08-31 15:47:19 -0600

    added json method

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,9 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-      console.log("inside", response);
+  fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
     });
-    console.log("outside");
-  };
+  });
+}
   
   getUserRepos();
